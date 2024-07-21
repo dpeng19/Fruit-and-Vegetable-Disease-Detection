1117dc5b8500f5fb5660619d237f3c34241e6d94
Derek <23depeng09@gmail.com>
2024-07-21 17:18:06 -0400

    Added code for random forest classifier.

--- disease_detection.py
+++ disease_detection.py
@@ -6,13 +6,17 @@ import numpy as np
 import keras
 import glob
 from PIL import Image
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, f1_score
 
 num_images = 1500    #number of images from each class that will be used
-SIZE = 64
+SIZE = 32   #resize images to this size
 
-dataset = []
-label = []
-labelCount = 0
+dataset = []   #list of images
+label = []     #list of labels
+labelCount = 0   #0-9, for 10 classes, 1st class will have label 0
 
 '''adds a random sample of 1500 images from each
    class into the dataset list, resizing each image,
@@ -38,6 +42,8 @@ def load_images(dir_path):
         label.append(labelCount)
     labelCount += 1
 
+
+#load fruit images
 load_images('Apple__Healthy/*')
 load_images('Apple__Rotten/*')
 load_images('Banana__Healthy/*')
@@ -48,6 +54,52 @@ load_images('Orange__Healthy/*')
 load_images('Orange__Rotten/*')
 load_images('Strawberry__Healthy/*')
 load_images('Strawberry__Rotten/*')
+
+def svm_model():
+    # flatten dataset
+    temp = np.array(dataset)
+    flattened_dataset = temp.reshape(len(dataset), -1)
+
+    # split data (80% training, 20% testing)
+    x_train, x_test, y_train, y_test = (train_test_split(flattened_dataset, label,
+                                                         train_size=0.8, random_state=42))
+
+    # create classifier using default params
+    classifier = SVC()
+    # fit model to training data
+    classifier.fit(x_train, y_train)
+    # predict
+    y_pred = classifier.predict(x_test)
+
+    # model accuracy
+    print('Accuracy:', accuracy_score(y_test, y_pred))
+    print('F1 Score:', f1_score(y_test, y_pred, average='macro'))
+
+
+# run model
+svm_model()
+
+def random_forest_model():
+    # flatten dataset
+    temp = np.array(dataset)
+    flattened_dataset = temp.reshape(len(dataset), -1)
+
+    # split data (80% training, 20% testing)
+    x_train, x_test, y_train, y_test = (train_test_split(flattened_dataset, label,
+                                                         train_size=0.8, random_state=42))
+
+    # create classifier using default params
+    classifier = RandomForestClassifier(n_estimators=100)
+
+    # fit model to training data
+    classifier.fit(x_train, y_train)
+    # predict
+    y_pred = classifier.predict(x_test)
+
+    # model accuracy
+    print('Accuracy:', accuracy_score(y_test, y_pred))
+    print('F1 Score:', f1_score(y_test, y_pred, average='macro'))
+random_forest_model()
 
 
 
